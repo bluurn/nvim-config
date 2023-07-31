cf02d43208e72760f04f649a8a1faea1e93f25b5
vladimir.suvorov <vladimir.suvorov@xempus.com>
2023-07-31 12:15:33 +0200

    enable autocompletion based on lsp

--- init.lua
+++ init.lua
@@ -1,41 +1,13 @@
--- TODO add completion
 -- TODO ripgrep
 -- TODO auto comments
 -- TODO add telescope
 -- TODO git
--- TODO tidy
 require("my.settings")
 require("my.keymaps")
 require("ext.lazy")
-require("ext/lsp")
-
-require('lazy').setup({
-    {
-        "kylechui/nvim-surround",
-        version = "*",
-        event = "VeryLazy",
-        config = function()
-            require("nvim-surround").setup({})
-        end
-    },
-    {
-        "nvim-tree/nvim-tree.lua",
-        version = "*",
-        lazy = false,
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-        },
-        config = function()
-            require("nvim-tree").setup {}
-        end,
-    },
-    { "nvim-treesitter/nvim-treesitter" },
-    { "neovim/nvim-lspconfig" },
-    { "williamboman/mason.nvim" },
-    { "williamboman/mason-lspconfig.nvim" },
-})
+require("ext.lsp")
+require("ext.completion")
 
 require("nvim-tree").setup()
 require("mason").setup()
 require("mason-lspconfig").setup()
-
--- lua/ext/lazy.lua
+++ lua/ext/lazy.lua
@@ -10,3 +10,35 @@ if not vim.loop.fs_stat(lazypath) then
     })
 end
 vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+    {
+        "kylechui/nvim-surround",
+        version = "*",
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({})
+        end
+    },
+    {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("nvim-tree").setup {}
+        end,
+    },
+    { "nvim-treesitter/nvim-treesitter" },
+    { "neovim/nvim-lspconfig" },
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
+    { "hrsh7th/nvim-cmp" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "saadparwaiz1/cmp_luasnip" },
+    { "L3MON4D3/LuaSnip" },
+}
+
+require('lazy').setup(plugins)
