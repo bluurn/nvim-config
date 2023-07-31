97e18181c3dc3d68e04bf0b0c4dce880314d4af6
vladimir.suvorov <vladimir.suvorov@xempus.com>
2023-07-31 09:08:46 +0200

    add lazy, add nvim surround

--- init.lua
+++ init.lua
@@ -2,3 +2,17 @@ require('my.settings')
 require('my.quickfix_toggle')
 require('my.netrw_toggle')
 require('my.keymaps')
+require('ext.lazy')
+
+require('lazy').setup({
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    }
+})
